f74a0abebb526d0eb30859213d1966899973a309
Z User <z@container>
2026-04-04 22:50:27 +0000

    fix: remove Firebase temporarily from Windows build
    - Remove firebase_core, firebase_analytics, firebase_messaging from pubspec.yaml
    - Remove Firebase initialization from main.dart
    - Keep google-services.json and Android config for future re-enable

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'providers/theme_provider.dart';
 import 'providers/vehicle_provider.dart';
 import 'providers/maintenance_provider.dart';
@@ -18,19 +16,9 @@ import 'screens/add_vehicle_screen.dart';
 import 'screens/add_maintenance_screen.dart';
 import 'screens/vehicle_details_screen.dart';
 
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-}
-
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
-  // Setup Firebase Messaging
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  await FirebaseMessaging.instance.requestPermission();
 
   runApp(
     MultiProvider(
