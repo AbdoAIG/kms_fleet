d6c7b0f8b7fb5ea201c93c7b5194d4832b016d11
Z User <z@container>
2026-04-08 16:57:00 +0000

    chore: remove 13 unused Dart files + 1 unused asset
    Deleted files that are not imported by any active code:
    - Screens: expenses_screen, add_expense_screen, checklist_screen, add_checklist_screen, register_screen
    - Services: db_native (old SQLite), db_stub, supabase_sync_service (never wired)
    - Widgets: stat_card (never used), vehicle_card (replaced by vehicle_3d_card)
    - Providers: expense_provider, checklist_provider (only imported by deleted screens)
    - Utils: app_constants (only imported by deleted add_expense_screen)
    - Asset: kms_logo_header.png (zero references)
    
    Kept: models/expense.dart and models/checklist.dart (still used by database_service and report_service)

--- flutter-app/lib/utils/app_constants.dart
+++
@@ -1,3 +0,0 @@
-/// Re-exports expense constants from constants.dart
-/// This file exists because add_expense_screen.dart imports from app_constants.dart
-export 'constants.dart' show AppConstants;
