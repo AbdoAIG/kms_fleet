b37bde230650ac2dbddb387091fa509ac0eff15a
AbdoAIG <86815412+AbdoAIG@users.noreply.github.com>
2026-04-09 19:06:42 +0200

    fix: upgrade Gradle/AGP for build compatibility

--- flutter-app/lib/screens/fuel_screen.dart
+++ flutter-app/lib/screens/fuel_screen.dart
@@ -24,7 +24,9 @@ class _FuelScreenState extends State<FuelScreen> {
   @override
   void initState() {
     super.initState();
-    _loadData();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadData();
+    });
   }
 
   Future<void> _loadData() async {
